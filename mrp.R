7835ec143eb947bf96c9b165ad620e1f7d7a0c9f
Jason Chan <jasonchan07@berkeley.edu>
2020-01-22 14:08:31 -0800

    combine load and clean functions

--- mrp.R
+++ mrp.R
@@ -1,13 +1,4 @@
-library(dplyr)
-library(janitor)
-library(tidyr)
-library(ggplot2)
-library(data.table)
-library(tidycensus)
-library(drake)
-library(visNetwork)
 
-setwd('/Users/jasonchan/Documents/DataProjects/mrp')
 
 bucket_ages <- function(birth_year) {
     # ````
@@ -25,41 +16,43 @@ bucket_ages <- function(birth_year) {
     return(buckets)
 }
 
-clean_cces_data <- function(cces_data, cols, col_names) {
+load_cces_data <- function(file, cols, col_names) {
+    # ``````
+    # loads cces data in csv, selects and renames demographic columns
+    # returns a data frame
+    # ``````
+    data = data.table::fread(file, data.table = FALSE,
+                             cols = cols, col.names = col_names)
+    
+    return(data)
+}
+
+clean_cces_data <- function() {
     # ````
-    # select, rename and transform columns from cces data
-    # returns a dataframe at the individual respondent level
+    # transforms columns from cces data
+    # returns a data frame
     # ````
-    data = cces_data %>% 
-        select(!!cols) %>% # select columns
-        setNames(col_names) %>% # rename columns
-        mutate(approval = ifelse(approval <= 2, 1, 0)) %>% # encode approvals as binary option 
-        mutate_at(vars(-birth_year), as.factor,) %>%  # convert all columns except birth year to factors
-        mutate(age = bucket_ages(birth_year)) %>% # convert birth years to age buckets
-        select(-birth_year) %>% 
-        arrange(state)
+    # select columns, rename columns
+    cces_data = load_cces_data(file = 'cces18.csv', cols = c('region', 'inputstate_post','birthyr','gender',
+                                                             'race', 'faminc_new', 'educ','CC18_308a'),
+                               col_names = c('region', 'state', 'birth_year', 'gender',
+                                             'race', 'income', 'education', 'approval')) %>% 
+    mutate(approval = ifelse(approval <= 2, 1, 0)) %>% # encode approvals as binary option 
+    mutate_at(vars(-birth_year), as.factor,) %>%  # convert all columns except birth year to factors
+    mutate(age = bucket_ages(birth_year)) %>% # convert birth years to age buckets
+    select(-birth_year) %>% 
+    arrange(state)
     
     return(data)
 }
 
 # data pipeline using drake
 data_pipeline <- drake_plan(
-    # read in raw cces data
-    raw_data = data.table::fread('cces18.csv', data.table = FALSE),
-    # clean cces data
-    data = raw_data %>% 
-        clean_cces_data(cols = c('region', 'inputstate_post',
-                                 'birthyr','gender',
-                                 'race', 'faminc_new',
-                                 'educ','CC18_308a'),
-                        col_names = c('region', 'state',
-                                      'birth_year', 'gender',
-                                      'race', 'income',
-                                      'education', 'approval')),
-    census_data = 
+    # load and clean raw cces data
+    cces_data = clean_cces_data() 
 )
                       
-make(data_pipeline)
+r_make(data_pipeline)
 vis <- drake_config(data_pipeline)
 readd(data)
 vis_drake_graph(vis)
@@ -82,7 +75,10 @@ female_counts <- paste('B01001_0', seq(31, 49), sep = '')
 female_counts_codes <- dput(as.character(female_counts))
 
 census_data <- get_acs(geography = 'state',
-                       variables = c(male_counts_codes, female_counts_codes)
+                       variables = c(male_counts = male_counts_codes, 
+                                     female_counts = female_counts_codes,
+                                     'B02001_002' = white_count,
+                                     )
 )
 
 
